4064a511a52935f686b70155200ed54855fec333
shamilkhan <shamilkhan1999@gmail.com>
2020-02-19 12:23:30 +0300

    initial commit

---
+++ 18-02-2020/Controllers/Files/Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+
+namespace _18_02_2020.Controllers.Files
+{
+    public class FileController
+    {
+        public async Task Post(HttpContext context)
+        {
+            await context.Response.WriteAsync("Hello from Post");
+        }
+    }
+}
